8f8fc8cad03e815238eef6214febac8ccec74663
Sue Duclos <sue.duclos@gmail.com>
2020-05-13 23:37:33 -0700

    finished lab

--- driver/driver.js
+++ driver/driver.js
@@ -8,3 +8,25 @@ const socket = new net.Socket();
 socket.connect({port:3000, host: 'localhost'}, () => {
   console.log('Connected to server');
 });
+
+socket.on('data', (payload) => {
+  let parsed = JSON.parse(payload.toString());
+
+  if (parsed.event === 'pickup') {
+
+    setTimeout(() => {
+      let newPayload = { event: 'in-transit', order: parsed.order};
+      console.log('picked up order', parsed.order.id);
+      socket.write(JSON.stringify(newPayload));
+    }, 1000);
+  }
+
+  if (parsed.event === 'in-transit') {
+
+    setTimeout(() => {
+      let newPayload = { event: 'delivered', order: parsed.order};
+      console.log('delivered order', parsed.order.id);
+      socket.write(JSON.stringify(newPayload));
+    }, 3000);
+  }
+});
