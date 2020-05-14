ab1b19144bd8cea0bba54c66784581b75fb62296
Sue Duclos <sue.duclos@gmail.com>
2020-05-13 18:15:32 -0700

    server up and running and sockets connected

--- csps/server.js
+++ csps/server.js
@@ -1,8 +1,4 @@
-// Main listener to driver and vendor
-// records all events, console.log that they happened and will broadcast the events to anything else connected to it
-
-// TCP Socket Server
-// receive connections
+'use strict';
 
 const net = require('net');
 const server = net.createServer();
@@ -10,4 +6,16 @@ const server = net.createServer();
 let port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log('Server is up and running on port', port);
+});
+
+let socketPool = [];
+
+const logger = (payload) => {
+  console.log('got', payload);
+};
+
+server.on('connection', (socket) => {
+  console.log('Socket connected to central server');
+  socketPool.push(socketPool);
+  socket.on('data', logger);
 });
--- driver/driver.js
+++ driver/driver.js
@@ -1,2 +1,10 @@
-// TCP Socket Connection 
-// wants to connect to a central server
+'use strict';
+
+// TCP Socket Connection connects to a central server
+
+const net = require('net');
+const socket = new net.Socket();
+
+socket.connect({port:3000, host: 'localhost'}, () => {
+  console.log('Connected to server');
+});
